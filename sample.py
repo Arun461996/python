5d8374c971203957e6953d8056c8372eace82f5a
ARUN461996 <arunprasath4696>
2024-09-05 12:56:21 +0000

    second

--- sample.py
+++ sample.py
@@ -11,7 +11,7 @@ if (year % 400 == 0) and (year % 100 == 0):
     print("{0} is a leap year".format(year))
 
 # not divided by 100 means not a century year
-# year divided by 4 is a leap year
+#year divided by 4 is a leap year
 elif (year % 4 ==0) and (year % 100 != 0):
     print("{0} is a leap year".format(year))
 
